266140e244643bfc3d52cb942f03983a010cd1ab
Scott Hardie <scottrmhardie@gmail.com>
2026-02-03 21:38:38 -0500

    feat: define oss/cloud distribution boundary

--- packages/contracts/src/types/index.ts
+++ packages/contracts/src/types/index.ts
@@ -2,5 +2,6 @@ export * from './jobs.js';
 export * from './runners.js';
 export * from './truth.js';
 export * from './common.js';
+export * from './distribution.js';
 export * from './registry.js';
 export * from './marketplace.js';
